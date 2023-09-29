cf58df79dfd382702702c4d78a73ff54099e7493
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2023-09-29 17:18:14 +0200

    Remove plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,7 @@
-import type { Config } from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme';
-import plugin from "tailwindcss/plugin";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-        title: ['Chivo', ...defaultTheme.fontFamily.sans],
-      },
-      animation: {
-        fadein: 'fadein 1s ease-in-out both',
-      },
-      keyframes: {
-        fadein: {
-          '0%': { opacity: '0', transform: 'translateY(20%)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
-        }
-      }
-    },
-  },
-  plugins: [
-    plugin(({ matchUtilities, theme }) => {
-      matchUtilities(
-        {
-          "animation-delay": (value) => {
-            return {
-              "animation-delay": value,
-            };
-          },
-        },
-        {
-          values: theme("transitionDelay"),
-        }
-      );
-    }),
-  ],
+  plugins: [],
 } satisfies Config
 
