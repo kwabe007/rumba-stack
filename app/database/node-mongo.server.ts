8bc2e352d7d01f7a4da5006d15b0cd2086dbfa09
kwabe007 <kwabe007@users.noreply.github.com>
2023-12-12 10:19:44 +0100

    Use getConnectionString functions in node-mongo.server.ts

--- app/database/node-mongo.server.ts
+++ app/database/node-mongo.server.ts
@@ -1,5 +1,5 @@
 import { Database } from '@paralect/node-mongo'
-import { createIndexIfNotExists } from './database-utils'
+import { createIndexIfNotExists, getConnectionString, getConnectionStringForPrint } from "./database-utils";
 import { requireEnvVar } from '~/utils.server'
 
 let db: Database
@@ -12,20 +12,8 @@ requireEnvVar('MONGODB_HOST')
 requireEnvVar('MONGODB_DATABASE')
 
 if (!global.__node_mongo_db__) {
-  // If neither user nor password is set, we assume access control is disabled, so we don't include the ':' and '@'
-  const userAuthPrintString =
-    process.env.MONGODB_USER || process.env.MONGODB_PASSWORD
-      ? `${process.env.MONGODB_USER}:********@`
-      : ''
-  const userAuthString = userAuthPrintString.replace(
-    /:\*+@/,
-    `:${process.env.MONGODB_PASSWORD}@`
-  )
-  const optionsString = process.env.MONGODB_OPTIONS
-    ? `/?${process.env.MONGODB_OPTIONS}`
-    : ''
-  const connectionPrintString = `mongodb://${userAuthPrintString}${process.env.MONGODB_HOST}${optionsString}`
-  const connectionString = `mongodb://${userAuthString}${process.env.MONGODB_HOST}${optionsString}`
+  const connectionPrintString = getConnectionStringForPrint()
+  const connectionString = getConnectionString()
   console.log(
     `Connecting to MongoDB with ${connectionPrintString} to database ${process.env.MONGODB_DATABASE}`
   )
