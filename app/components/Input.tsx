e010b165db24c700ec54a06cd3aba32adae93b1e
kwabe007 <kwabe007@users.noreply.github.com>
2024-01-14 11:55:59 +0100

    Update Input.tsx

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,40 +1,29 @@
-import { useField } from 'remix-validated-form'
 import { ComponentPropsWithoutRef } from 'react'
+import clsx from 'clsx'
+import { useField } from 'remix-validated-form'
 
 interface InputProps extends ComponentPropsWithoutRef<'input'> {
   name: string
   label?: string
 }
 
-export default function Input({
-                                name,
-                                label,
-                                type,
-                                className,
-                                ...rest
-                              }: InputProps) {
+export default function Input({ label, name, className, ...rest }: InputProps) {
   const { error } = useField(name)
 
   return (
     <div className={className}>
-      <div className="relative">
-        {label && (
-          <label
-            className="block text-sm font-medium text-gray-700"
-            htmlFor={name}
-          >
-            {label}
-          </label>
-        )}
-        <input
-          {...rest}
-          className="w-full rounded border-2 border-gray-400 px-2 py-1 text-lg"
-          id={name}
-          name={name}
-          type={type}
-        />
-        {error && <span className="text-red-700 text-sm">{error}</span>}
-      </div>
+      {label && (
+        <label className="block text-sm font-medium text-gray-700" htmlFor={name}>
+          {label}
+        </label>
+      )}
+      <input
+        {...rest}
+        className="w-full rounded border-2 border-gray-400 px-2 py-1 text-lg"
+        id={name}
+        name={name}
+      />
+      {error && <span className="text-red-700 text-sm">{error}</span>}
     </div>
   )
 }
