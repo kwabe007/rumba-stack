2a6097aff8630ecba4eb3397a1430b9b31c0266b
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2024-01-26 22:52:41 +0100

    Add Outfit font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,5 +2,12 @@ import type { Config } from 'tailwindcss'
 
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", "Arial", "sans-serif"],
+      },
+    }
+  },
   plugins: [],
 } satisfies Config
